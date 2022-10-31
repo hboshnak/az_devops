82084023d91481d9e12ab0076f16a4d0ac8f5674
Huben Boshnakov <huben.boshnakov@gmail.com>
2022-10-31 15:15:15 +0000

    Add cli example from cloud shell

---
+++ main.py
@@ -0,0 +1,16 @@
+import click
+
+@click.command(help="This is a Click example app.")
+@click.option("--name", prompt="Name to be defined", help="I need a name")
+@click.option("--color", prompt="Color to be defined", help="I need a color")
+def main(name, color):
+    if name == "Mani":
+        click.echo("Mani, you are always black!")
+        click.echo(click.style(f"Hello {name}!", fg="black"))
+    else:
+        click.echo(f"Your color of choise is {color}!")
+        click.echo(click.style(f"Hello {name}!", fg=color))
+        pass
+
+if __name__ == "__main__":
+    main()
