a5a00455e6f054bcb24bfb2ff38e8f4d5db4b71e
Huben Boshnakov <huben.boshnakov@gmail.com>
2022-10-31 22:50:11 +0200

    Test commit to check Actions

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+"""Module demonstrating Click CLI
+"""
 import click
 
 @click.command(help="This is a Click example app.")
 @click.option("--name", prompt="Name to be defined", help="I need a name")
 @click.option("--color", prompt="Color to be defined", help="I need a color")
 def main(name, color):
+    """Basic Click example
+    """
     if name == "Mani":
         click.echo("Mani, you are always black!")
         click.echo(click.style(f"Hello {name}!", fg="black"))
     else:
         click.echo(f"Your color of choise is {color}!")
         click.echo(click.style(f"Hello {name}!", fg=color))
-        pass
 
 if __name__ == "__main__":
     main()
